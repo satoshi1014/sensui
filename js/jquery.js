3a9384a351890367d50c73c93a526b6b2da6adb1
EC2 Default User <ec2-user@ip-172-31-3-104.ap-northeast-1.compute.internal>
2021-03-20 05:08:02 +0000

    second commit

--- js/jquery.js
+++ js/jquery.js
@@ -1,8 +1,39 @@
 $(function(){
     $('.slick01').slick({ //{}を入れる
-        // autoplay: true, //「オプション名: 値」の形式で書く
+        autoplay: true, //「オプション名: 値」の形式で書く
         dots: true, //複数書く場合は「,」でつなぐ
-        // autoplaySpeed:5000,
+        autoplaySpeed:5000,
     });
-  
+  var $nav   = $('#navArea');
+  var $btn   = $('.toggle_btn');
+  var $mask  = $('#mask, .inner ul li a');
+  var open   = 'open'; // class
+  // menu open close
+  $btn.on( 'click', function() {
+    if ( ! $nav.hasClass( open ) ) {
+      $nav.addClass( open );
+    } else {
+      $nav.removeClass( open );
+    }
+  });
+  // mask close
+  $mask.on('click', function() {
+    $nav.removeClass( open );
+  });
+   $('#header-list li a, .inner ul li a, #main-image-list li a').click(function(){
+        var id = $(this).attr('href');
+        var position = $(id).offset().top;
+        $('html, body').animate({
+            'scrollTop': position
+        }, 500);
+    });
+    $('#header-title a').click(function(){
+        $('html, body').animate({
+          'scrollTop': 0
+        }, 500);
+    });
+    window.onload = function() {
+      const spinner = document.getElementById('loading');
+      spinner.classList.add('loaded');
+    }
 });
