430ec31f35d37c9d280f1e756e15b5ac326fe5cd
EC2 Default User <ec2-user@ip-172-31-3-104.ap-northeast-1.compute.internal>
2021-03-20 03:20:49 +0000

    first commit

---
+++ js/jquery.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.slick01').slick({ //{}を入れる
+        // autoplay: true, //「オプション名: 値」の形式で書く
+        dots: true, //複数書く場合は「,」でつなぐ
+        // autoplaySpeed:5000,
+    });
+  
+});
